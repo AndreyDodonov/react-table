5374542db3efa70818bfcc8efc2ba6e87cb8cddc
Andrew Dodonov <nordhummer@gmail.com>
2021-11-26 15:45:56 +0400

    :bug: add cath in fetch

--- src/App.js
+++ src/App.js
@@ -54,13 +54,16 @@ class App extends React.Component {
      */
 
     getTableInfo = async (numberRows) => {
-        const apiUrl = await fetch(`http://www.filltext.com/?rows=${numberRows}&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D`);
-        const data = await apiUrl.json(); // TODO: <-- Добавить catch и обработку ошибок
-
-        this.setState({
+        
+        await fetch(`http://www.filltext.com/?rows=${numberRows}&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D`)
+        .then(response => response.json())
+        .then(data => this.setState({
             isLoading: false,
             data: _.orderBy(data, this.state.sortedField, this.state.sortDirection)
-        })
+        }))    
+          .catch(err => {
+            throw new Error(err)
+          })
     };
 
     /**
